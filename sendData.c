8eda756aa7f98e90458756e55499bd2d02b69714
Stefan Alexandru <alex.stefan007@gmail.com>
2018-11-04 23:00:30 +0200

    Add files via upload

---
+++ sendData.c
@@ -0,0 +1,42 @@
+        #include <stdio.h>
+	#include <stdlib.h>
+	#include <errno.h>
+	#include <string.h>
+	#include <fcntl.h>
+	#include <sys/types.h>
+	#include <sys/stat.h>
+	#include <unistd.h>
+
+    main()
+    {
+        char s[300];
+        int num, fd;
+        printf("Astept cititori...\n");
+        printf("Am un cititor....introduceti ceva...\n");
+	pid_t pid = fork();
+	while(1==1)
+	{
+		if(pid!=0)
+		{
+			fd=open("/fenrir/studs/dana.paduraru/retele/Chat_Dana", O_WRONLY);
+			gets(s);
+          		if ((num = write(fd, s, strlen(s))) == -1)
+                		perror("Problema la scriere in FIFO!");
+			close(fd);
+		}
+		else
+		{
+			fd = open("/fenrir/studs/dana.paduraru/retele/Chat_Alex", O_RDONLY);
+			do {
+            			if ((num = read(fd, s, 300)) == -1)
+                			perror("Eroare la citirea din FIFO!");
+            			else {
+                			s[num] = '\0';
+					printf("Dana:  \"%s\"\n", s);
+            			     }
+        		} while (num > 0);
+			close(fd);
+		}
+	}
+    }
+
